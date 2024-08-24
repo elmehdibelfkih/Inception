f655ee816bd1c1a06063e286812288cb81b3b658
elmehdibelfkih <elmehdibelfkih@gmail.com>
2024-08-24 01:24:50 -0400

    start with wordpress

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 up:
+	unzip -o ./srcs/requirements/wordpress/tools/wordpress-6.6.1.zip -d ./srcs/requirements/wordpress/tools
 	docker compose -f ./srcs/docker-compose.yml up -d
+	rm -rf ./srcs/requirements/wordpress/tools/wordpress
 down:
 	docker compose -f ./srcs/docker-compose.yml down
 clean: down
--- srcs/requirements/mariadb/conf/docker-entrypoint.sh
+++ srcs/requirements/mariadb/conf/docker-entrypoint.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
-
-# Secure the installation by executing SQL commands directly
 set -e
 
-mysqld_safe --skip-grant-tables
-
+service mysql start
 mysql -u root <<EOF
--- Set the root password
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-
--- Remove anonymous users
 DELETE FROM mysql.user WHERE User='';
-
 -- Disallow root login remotely
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
-
--- Remove the test database
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-
-CREATE USER '$MYSQL_USER'@'host' IDENTIFIED BY '$MYSQL_PASSWORD';
-
-GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;
-CREATE DATABASE $MYSQL_DATABASE;
-
-
-
-
-
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
 
 -- Reload privilege tables
 FLUSH PRIVILEGES;
 
-
-
 EOF
-tail -f /dev/null
