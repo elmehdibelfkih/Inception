80ef16378828a578f618e56fa1838737bb978355
mehdi <mehdibm.az@gmail.com>
2024-08-28 13:31:53 +0000

    save changes

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 up:
-	unzip -o ./srcs/requirements/wordpress/tools/wordpress-6.6.1.zip -d ./srcs/requirements/wordpress/tools
+	unzip -o ./srcs/requirements/wordpress/tools/wordpress-6.6.1.zip -d ./srcs/requirements/wordpress/tools | tail -n 20
 	docker compose -f ./srcs/docker-compose.yml up -d
 	rm -rf ./srcs/requirements/wordpress/tools/wordpress
 down:
-	docker compose -f ./srcs/docker-compose.yml down
+	docker compose -f ./srcs/docker-compose.yml down -v
 clean: down
 	docker system prune -a 
 	docker volume prune -f
-re: clean up
+re: down up
+rebuild: clean up
--- srcs/requirements/mariadb/conf/mariaDB.sh
+++ srcs/requirements/mariadb/conf/mariaDB.sh
@@ -1,21 +1,51 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
 
+set -e
 service mysql start
-mysql -u root <<EOF
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+
+until mysqladmin ping --silent > /dev/null 2>&1; do # Hadi raha khadama
+  echo "Waiting for MySQL to start..."
+  sleep 2
+done
+# mysqld_safe --skip-grant-tables &
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD";
 DELETE FROM mysql.user WHERE User='';
 -- Disallow root login remotely
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
-
+CREATE USER IF NOT EXISTS "$MYSQL_USER"@'localhost' IDENTIFIED BY "$MYSQL_PASSWORD";
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO "$MYSQL_USER"@'localhost';
 -- Reload privilege tables
 FLUSH PRIVILEGES;
 EOF
+# mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+mysqld_safe --user=mysql > /dev/null 2>&1 # Hta hadi
 
-mysqld
-tail -f /dev/null
+
+# set -e
+
+
+# echo "Waiting for MariaDB to be ready..."
+# # Wait for MariaDB to start
+# until mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute="SELECT 1" > /dev/null 2>&1; do
+#     echo "MariaDB not yet available, retrying..."
+#     sleep 2
+# done
+
+# echo "MariaDB is up. Starting database setup..."
+
+# # Create a new database and user
+# mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<EOF
+# CREATE DATABASE IF NOT EXISTS $DB_NAME;
+# CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
+# -- Flush privileges
+# FLUSH PRIVILEGES;
+# EOF
+
+# echo "Database and user created successfully."
+# mysqld_safe --user=mysql
